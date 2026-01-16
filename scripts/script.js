d9d91d4d82e43f7f6eae607bf00a35a9f0f85777
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-01-17 00:12:23 +0100

    Refarctoring and commentating

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,33 @@
-const { createElement } = require("react")
-
+/*
+    Function that loads the informations from the API from the latitude and the longitude given by the user.
+*/
 async function loadInformations(){
     let latitude = parseFloat(document.querySelector("#latitudeInput").value)
     let longitude = parseFloat(document.querySelector("#longitudeInput").value)
     let datas = document.querySelector("#datas")
 
+    //Checking if the values given by the user are corrects
     if(!isNaN(latitude) && !isNaN(longitude)){ 
+
+        //Getting all the info from the API call
         const p = await getTemperatureAtLocation(latitude, longitude)
-        console.log(p)
         let dataLine = null
+
+        //Browsing all the datas. They are given in the object hourly that is {time:array, temperature_2m:array} 
         for(let i = 0; i < p.hourly.time.length; i++){
+            //Creating the elements
             dataLine = document.createElement("div")
             let time = document.createElement("p")
-            time.textContent = p.hourly.time[i]
             let temperature = document.createElement("p")
-            temperature.textContent = `${p.hourly.temperature_2m[i]} °C`
             dataLine.classList.add("dataLine")
+
+            //Adding the corresponding value to the elements
+            time.textContent = p.hourly.time[i]
+            temperature.textContent = `${p.hourly.temperature_2m[i]} °C`
+
+            //Adding the elements to the page
             dataLine.appendChild(time)
             dataLine.appendChild(temperature)
-            
             datas.appendChild(dataLine)
         }
 

2ebaebaeaf5250759c22bc83dc129c070a1b770a
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-01-20 14:13:12 +0100

    Function completed

--- scripts/DayValues.js
+++ scripts/DayValues.js
@@ -1,16 +1,16 @@
 class DayValues{
     /*
-        d: A string that contain the day concerned by the values
+        d: A string that contain the day concerned by the values, in the UTC format
         ti: An array that contains all the possible that are displayed
         val: An that contains the values of the day that the user already requested. The keys are the name of the values in the API.
 
         The index of the arrays are matched, the i value correspond to the i time.
     */
-    constructor(d, dispVal, ti, val){
+    constructor(d, ti, val, un){
         this.day = d
-        this.valueDisplayed = dispVal
         this.time = ti
         this.values = val
+        this.unit = un
 
     }
 

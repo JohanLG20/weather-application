7c69608d68cf09c6ba0e131d5726822ca20ca369
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-02-04 13:49:56 +0100

    Data type added and text is now in table

--- scripts/DayValues.js
+++ scripts/DayValues.js
@@ -2,16 +2,14 @@ class DayValues{
     /*
         d: A string that contain the day concerned by the values, in the UTC format
         ti: An array that contains all the possible that are displayed
-        val: An that contains the values of the day that the user already requested. The keys are the name of the values in the API.
+        val: An array that contains the values of the day that the user already requested. 
 
         The index of the arrays are matched, the i value correspond to the i time.
     */
-    constructor(d, ti, val, un, di){
+    constructor(d, ti, val){
         this.day = d
         this.time = ti
         this.values = val
-        this.unit = un
-        this.unitDisplayed = di
 
     }
 
--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -7,15 +7,24 @@
 * @returns An object containing the humidity values. Can throw an error if the API is unreachable
 */
 async function getValuesAtLocation(latitude, longitude, valueToGet) {
-    
-    const allInfos = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=${valueToGet}`)
 
-    if (allInfos.ok) {
-        return allInfos.json()
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=${valueToGet}`)
+
+        if (!response.ok) {
+            throw new Error(`Impossible de charger les données. Code : ${response.status}`)
+        }
+
+        const allInfos = await response.json()
+
+        return allInfos
+
     }
-    else {
-        throw new Error("Impossible de charger les données")
+    catch(err){
+        console.log(err)
     }
+    
+    
 
 }
 
